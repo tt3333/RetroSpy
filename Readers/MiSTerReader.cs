7080d84f4dd4a79e4ee107ade78aa8ad8c3cf630
Christopher J. Mallery <christopher.mallery@gmail.com>
2019-12-10 21:10:27 -0800

    Trying to do some cleanup

--- Readers/MiSTerReader.cs
+++ Readers/MiSTerReader.cs
@@ -8,58 +8,90 @@ namespace RetroSpy.Readers
 {
     static public class MiSTerReader
     {
-        const int PRO_PACKET_SIZE = 268;
 
-        static readonly string[] BUTTONS = {
-            "b", "a", "y", "x", "l", "r", "select", "start", "9", "10", "11"
+        static readonly string[] AXES_NAMES = {
+            "x", "y", "z", "rx", "ry", "rz", "s0", "s1"
         };
 
 
         static public ControllerState ReadFromPacket(byte[] packet)
         {
-            if (packet.Length != PRO_PACKET_SIZE) return null;
+            if (packet.Length < 16) return null;
 
-            byte[] polishedPacket = new byte[BUTTONS.Length];
-            int[] intPacket = new int[8];
-
-            for (int i = 0; i < 11; ++i)
-                polishedPacket[i] = (byte)((packet[i] == 0x31) ? 1 : 0);
-
-            for (int i = 0; i < 8; ++i)
+            int axes = 0;
+            for (byte j = 0; j < 8; ++j)
             {
-                intPacket[i] = 0;
+                axes |= (int)((packet[j] == 0x30 ? 0 : 1) << j);
+            }
+
+            int buttons = 0;
+            for (byte j = 0; j < 8; ++j)
+            {
+                buttons |= (int)((packet[8+j] == 0x30 ? 0 : 1) << j);
+            }
+
+            int packetSize = 2 + (axes * 32) + buttons;
+
+            if (packet.Length != packetSize) return null;
+
+            byte[] buttonValues = new byte[buttons];
+            int[] axesValues = new int[axes];
+
+            for (int i = 0; i < buttons; ++i)
+                buttonValues[i] = (byte)((packet[i] == 0x31) ? 1 : 0);
+
+            for (int i = 0; i < axes; ++i)
+            {
+                axesValues[i] = 0;
                 for (byte j = 0; j < 32; ++j)
                 {
-                    intPacket[i] |= (int)((packet[11 + (i * 32 + j)] == 0x30 ? 0 : 1) << j);
+                    axesValues[i] |= (packet[buttons + i * 32 + j] == 0x30 ? 0 : 1) << j;
                 }
             }
 
             var outState = new ControllerStateBuilder();
 
-            for (int i = 0; i < BUTTONS.Length; ++i)
+            for (int i = 0; i < buttonValues.Length; ++i)
             {
-                if (string.IsNullOrEmpty(BUTTONS[i])) continue;
-                outState.SetButton(BUTTONS[i], polishedPacket[i] != 0x00);
+                outState.SetButton("b" + i.ToString(), buttonValues[i] != 0x00);
             }
 
-            if (intPacket[6] < 0)
-                outState.SetButton("left", true);
-            else if (intPacket[6] > 0)
-                outState.SetButton("right", true);
-            else
+            for(int i = 0; i < axesValues.Length; ++i)
             {
-                outState.SetButton("left", false);
-                outState.SetButton("right", false);
+                if (i >= AXES_NAMES.Length)
+                    outState.SetAnalog(AXES_NAMES[i], axesValues[i] / short.MaxValue);
+
+                outState.SetAnalog("a" + i, axesValues[i] / short.MaxValue);
             }
 
-            if (intPacket[7] < 0)
-                outState.SetButton("up", true);
-            else if (intPacket[7] > 0)
-                outState.SetButton("down", true);
+            if (axes >= 2)
+            {
+                if (axesValues[axes - 2] < 0)
+                    outState.SetButton("left", true);
+                else if (axesValues[axes - 2] > 0)
+                    outState.SetButton("right", true);
+                else
+                {
+                    outState.SetButton("left", false);
+                    outState.SetButton("right", false);
+                }
+
+                if (axesValues[axes - 1] < 0)
+                    outState.SetButton("up", true);
+                else if (axesValues[axes - 1] > 0)
+                    outState.SetButton("down", true);
+                else
+                {
+                    outState.SetButton("up", false);
+                    outState.SetButton("down", false);
+                }
+            }
             else
             {
                 outState.SetButton("up", false);
                 outState.SetButton("down", false);
+                outState.SetButton("left", false);
+                outState.SetButton("right", false);
             }
 
             return outState.Build();
