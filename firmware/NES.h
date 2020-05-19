0247ed6b495dd2a6fab06f8285b74e3d12a8eca7
Zoggins <zoggins@gmail.com>
2020-05-19 14:32:59 -0700

    more code cleanup

--- firmware/NES.h
+++ firmware/NES.h
@@ -5,7 +5,6 @@
 // The only reason you'd want to use 2-wire mode is if you built a NintendoSpy
 // before the 3-wire firmware was implemented.  This mode is for backwards
 // compatibility only.
-//#define MODE_2WIRE_SNES
 //#define MODE_2WIRE_NES
 // ---------------------------------------------------------------------------------
 
--- firmware/SNES.h
+++ firmware/SNES.h
@@ -1,6 +1,13 @@
 #ifndef SNESSpy_h
 #define SNESSpy_h
 
+// ---------------------------------------------------------------------------------
+// The only reason you'd want to use 2-wire mode is if you built a NintendoSpy
+// before the 3-wire firmware was implemented.  This mode is for backwards
+// compatibility only.
+//#define MODE_2WIRE_SNES
+// ---------------------------------------------------------------------------------
+
 #include "ControllerSpy.h"
 
 class SNESSpy : public ControllerSpy {
