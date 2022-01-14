044509127cbaeb38b221179ef85ea41eb411607f
Chris Mallery <zoggins@retro-spy.com>
2022-01-13 17:32:12 -0800

    Adding config files to .zip and installer

--- release.bat
+++ release.bat
@@ -116,87 +116,120 @@ if exist "..\..\..\..\certs\codesign.pfx" (
 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\SignTool" sign /f "..\..\..\..\certs\codesign.pfx" /p %codesignpasswd% /tr http://timestamp.comodoca.com  /td sha256 /a Retrospy.exe
 )
 "C:\Program Files\7-Zip\7z.exe" a ..\..\RetroSpy-release.zip RetroSpy.exe
+"C:\Program Files\7-Zip\7z.exe" a ..\..\RetroSpy-release.zip RetroSpy.exe.config
 copy RetroSpy.exe ..\..\RetroSpy-Setup
+copy RetroSpy.exe.config ..\..\RetroSpy-Setup
 
 if exist "..\..\..\..\certs\codesign.pfx" (
 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\SignTool" sign /f "..\..\..\..\certs\codesign.pfx" /p %codesignpasswd% /tr http://timestamp.comodoca.com  /td sha256 /a GBPemu.exe
 )
 "C:\Program Files\7-Zip\7z.exe" a ..\..\RetroSpy-release.zip GBPemu.exe
+"C:\Program Files\7-Zip\7z.exe" a ..\..\RetroSpy-release.zip GBPemu.exe.config
 copy GBPemu.exe ..\..\RetroSpy-Setup
+copy GBPemu.exe.config ..\..\RetroSpy-Setup
 
 if exist "..\..\..\..\certs\codesign.pfx" (
 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\SignTool" sign /f "..\..\..\..\certs\codesign.pfx" /p %codesignpasswd% /tr http://timestamp.comodoca.com  /td sha256 /a UsbUpdater.exe
 )
 "C:\Program Files\7-Zip\7z.exe" a ..\..\RetroSpy-release.zip UsbUpdater.exe
+"C:\Program Files\7-Zip\7z.exe" a ..\..\RetroSpy-release.zip UsbUpdater.exe.config
 copy UsbUpdater.exe ..\..\RetroSpy-Setup
+copy UsbUpdater.exe.config ..\..\RetroSpy-Setup
 
 if exist "..\..\..\..\certs\codesign.pfx" (
 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\SignTool" sign /f "..\..\..\..\certs\codesign.pfx" /p %codesignpasswd% /tr http://timestamp.comodoca.com  /td sha256 /a GBPUpdater.exe
 )
 "C:\Program Files\7-Zip\7z.exe" a ..\..\RetroSpy-release.zip GBPUpdater.exe
+"C:\Program Files\7-Zip\7z.exe" a ..\..\RetroSpy-release.zip GBPUpdater.exe.config
 copy GBPUpdater.exe ..\..\RetroSpy-Setup
+copy GBPUpdater.exe.config ..\..\RetroSpy-Setup
 
 cd ..\..
 
 cd bin\x64\Release
 copy RetroSpy.exe RetroSpy.x64.exe
+copy RetroSpy.exe.config RetroSpy.x64.exe.config
 if exist "..\..\..\..\..\certs\codesign.pfx" (
 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\SignTool" sign /f "..\..\..\..\..\certs\codesign.pfx" /p %codesignpasswd% /tr http://timestamp.comodoca.com  /td sha256 /a Retrospy.x64.exe
 )
 "C:\Program Files\7-Zip\7z.exe" a ..\..\..\RetroSpy-release.zip RetroSpy.x64.exe
+"C:\Program Files\7-Zip\7z.exe" a ..\..\..\RetroSpy-release.zip RetroSpy.x64.exe.config
 copy RetroSpy.x64.exe ..\..\..\RetroSpy-Setup\
+copy RetroSpy.x64.exe.config ..\..\..\RetroSpy-Setup\
 
 copy GBPemu.exe GBPemu.x64.exe
+copy GBPemu.exe.config GBPemu.x64.exe.config
 if exist "..\..\..\..\..\certs\codesign.pfx" (
 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\SignTool" sign /f "..\..\..\..\..\certs\codesign.pfx" /p %codesignpasswd% /tr http://timestamp.comodoca.com  /td sha256 /a GBPemu.x64.exe
 )
 "C:\Program Files\7-Zip\7z.exe" a ..\..\..\RetroSpy-release.zip GBPemu.x64.exe
+"C:\Program Files\7-Zip\7z.exe" a ..\..\..\RetroSpy-release.zip GBPemu.x64.exe.config
 copy GBPemu.x64.exe ..\..\..\RetroSpy-Setup\
+copy GBPemu.x64.exe.config ..\..\..\RetroSpy-Setup\
+
 
 copy UsbUpdater.exe UsbUpdater.x64.exe
+copy UsbUpdater.exe UsbUpdater.x64.exe.config
 if exist "..\..\..\..\..\certs\codesign.pfx" (
 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\SignTool" sign /f "..\..\..\..\..\certs\codesign.pfx" /p %codesignpasswd% /tr http://timestamp.comodoca.com  /td sha256 /a UsbUpdater.x64.exe
 )
 "C:\Program Files\7-Zip\7z.exe" a ..\..\..\RetroSpy-release.zip UsbUpdater.x64.exe
+"C:\Program Files\7-Zip\7z.exe" a ..\..\..\RetroSpy-release.zip UsbUpdater.x64.exe.config
 copy UsbUpdater.x64.exe ..\..\..\RetroSpy-Setup\
+copy UsbUpdater.x64.exe.config ..\..\..\RetroSpy-Setup\
 
 copy GBPUpdater.exe GBPUpdater.x64.exe
+copy GBPUpdater.exe GBPUpdater.x64.exe.config
 if exist "..\..\..\..\..\certs\codesign.pfx" (
 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\SignTool" sign /f "..\..\..\..\..\certs\codesign.pfx" /p %codesignpasswd% /tr http://timestamp.comodoca.com  /td sha256 /a GBPUpdater.x64.exe
 )
 "C:\Program Files\7-Zip\7z.exe" a ..\..\..\RetroSpy-release.zip GBPUpdater.x64.exe
+"C:\Program Files\7-Zip\7z.exe" a ..\..\..\RetroSpy-release.zip GBPUpdater.x64.exe.config
 copy GBPUpdater.x64.exe ..\..\..\RetroSpy-Setup\
+copy GBPUpdater.x64.exe.config ..\..\..\RetroSpy-Setup\
 
 cd ..\..\..
 
 cd bin\x86\Release
 copy RetroSpy.exe RetroSpy.x86.exe
+copy RetroSpy.exe.config RetroSpy.x86.exe.config
 if exist "..\..\..\..\..\certs\codesign.pfx" (
 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\SignTool" sign /f "..\..\..\..\..\certs\codesign.pfx" /p %codesignpasswd% /tr http://timestamp.comodoca.com  /td sha256 /a Retrospy.x86.exe
 )
 "C:\Program Files\7-Zip\7z.exe" a ..\..\..\RetroSpy-release.zip RetroSpy.x86.exe
+"C:\Program Files\7-Zip\7z.exe" a ..\..\..\RetroSpy-release.zip RetroSpy.x86.exe.config
 copy RetroSpy.x86.exe ..\..\..\RetroSpy-Setup\
+copy RetroSpy.x86.exe.config ..\..\..\RetroSpy-Setup\
 
 copy GBPemu.exe GBPemu.x86.exe
+copy GBPemu.exe.config GBPemu.x86.exe.config
 if exist "..\..\..\..\..\certs\codesign.pfx" (
 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\SignTool" sign /f "..\..\..\..\..\certs\codesign.pfx" /p %codesignpasswd% /tr http://timestamp.comodoca.com  /td sha256 /a GBPemu.x86.exe
 )
 "C:\Program Files\7-Zip\7z.exe" a ..\..\..\RetroSpy-release.zip GBPemu.x86.exe
+"C:\Program Files\7-Zip\7z.exe" a ..\..\..\RetroSpy-release.zip GBPemu.x86.exe.config
 copy GBPemu.x86.exe ..\..\..\RetroSpy-Setup\
+copy GBPemu.x86.exe.config ..\..\..\RetroSpy-Setup\
 
 copy UsbUpdater.exe UsbUpdater.x86.exe
+copy UsbUpdater.exe.config UsbUpdater.x86.exe.config
 if exist "..\..\..\..\..\certs\codesign.pfx" (
 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\SignTool" sign /f "..\..\..\..\..\certs\codesign.pfx" /p %codesignpasswd% /tr http://timestamp.comodoca.com  /td sha256 /a UsbUpdater.x86.exe
 )
 "C:\Program Files\7-Zip\7z.exe" a ..\..\..\RetroSpy-release.zip UsbUpdater.x86.exe
+"C:\Program Files\7-Zip\7z.exe" a ..\..\..\RetroSpy-release.zip UsbUpdater.x86.exe.config
 copy UsbUpdater.x86.exe ..\..\..\RetroSpy-Setup\
+copy UsbUpdater.x86.exe.config ..\..\..\RetroSpy-Setup\
 
 copy GBPUpdater.exe GBPUpdater.x86.exe
+copy GBPUpdater.exe.config GBPUpdater.x86.exe.config
 if exist "..\..\..\..\..\certs\codesign.pfx" (
 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\SignTool" sign /f "..\..\..\..\..\certs\codesign.pfx" /p %codesignpasswd% /tr http://timestamp.comodoca.com  /td sha256 /a GBPUpdater.x86.exe
 )
 "C:\Program Files\7-Zip\7z.exe" a ..\..\..\RetroSpy-release.zip GBPUpdater.x86.exe
+"C:\Program Files\7-Zip\7z.exe" a ..\..\..\RetroSpy-release.zip GBPUpdater.x86.exe.config
 copy GBPUpdater.x86.exe ..\..\..\RetroSpy-Setup\
+copy GBPUpdater.x86.exe.config ..\..\..\RetroSpy-Setup\
 
 cd ..\..\..
 
