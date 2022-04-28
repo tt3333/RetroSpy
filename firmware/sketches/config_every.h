401643d34299c5f16cc6464135971ddaed961519
Chris Mallery <chmaller@microsoft.com>
2022-04-27 18:40:01 -0700

    check in

--- firmware/sketches/config_every.h
+++ firmware/sketches/config_every.h
@@ -26,118 +26,114 @@
 
 #define NOT_CONNECTED	   NA
 
-#define DIGITAL_PIN_00	   0
-#define DIGITAL_PIN_01	   1
-#define DIGITAL_PIN_02	   2
-#define DIGITAL_PIN_03	   3
-#define DIGITAL_PIN_04	   4
-#define DIGITAL_PIN_05	   5
-#define DIGITAL_PIN_06	   6
-#define DIGITAL_PIN_07	   7
-#define DIGITAL_PIN_08	   0
-#define DIGITAL_PIN_09	   1
-#define DIGITAL_PIN_10	   2
-#define DIGITAL_PIN_11	   3
+#define DIGITAL_PIN_17	   0
+#define DIGITAL_PIN_16	   1
+#define DIGITAL_PIN_15	   2
+#define DIGITAL_PIN_14	   3
+#define DIGITAL_PIN_20	   4
+#define DIGITAL_PIN_21	   5
+//#define DIGITAL_PIN_XX	   6
+//#define DIGITAL_PIN_XX	   7
+#define DIGITAL_PIN_02	   0
+#define DIGITAL_PIN_07	   1
+#define DIGITAL_PIN_18	   2
+#define DIGITAL_PIN_19	   3
 
-#define ANALOG_PIN_00	   0
-#define ANALOG_PIN_01	   1
-#define ANALOG_PIN_02	   2
+#define MODEPIN_SNES       3
+#define MODEPIN_N64        4
+#define MODEPIN_GC         5
+//#define MODEPIN_X        6
+//#define MODEPIN_Y        8
 
-#define PORTB_PIN_OFFSET   8
+#define N64_PIN	           DIGITAL_PIN_17
 
-#define MODEPIN_SNES       ANALOG_PIN_00
-#define MODEPIN_N64        ANALOG_PIN_01
-#define MODEPIN_GC         ANALOG_PIN_02
+#define SNES_LATCH         DIGITAL_PIN_16
+#define SNES_DATA          DIGITAL_PIN_15
+#define SNES_CLOCK         DIGITAL_PIN_20
 
-#define N64_PIN	           DIGITAL_PIN_02
+#define NES_LATCH          DIGITAL_PIN_16
+#define NES_CLOCK          DIGITAL_PIN_20
+#define NES_DATA           DIGITAL_PIN_15
+#define NES_DATA0          DIGITAL_PIN_17
+#define NES_DATA1          DIGITAL_PIN_14
 
-#define SNES_LATCH         DIGITAL_PIN_03
-#define SNES_DATA          DIGITAL_PIN_04
-#define SNES_CLOCK         DIGITAL_PIN_06
+#define GC_PIN             DIGITAL_PIN_14
 
-#define NES_LATCH          DIGITAL_PIN_03
-#define NES_CLOCK          DIGITAL_PIN_06
-#define NES_DATA           DIGITAL_PIN_04
-#define NES_DATA0          DIGITAL_PIN_02
-#define NES_DATA1          DIGITAL_PIN_05
+#define ThreeDO_LATCH      DIGITAL_PIN_17
+#define ThreeDO_DATA       DIGITAL_PIN_15
+#define ThreeDO_CLOCK      DIGITAL_PIN_16
 
-#define GC_PIN             DIGITAL_PIN_05
+#define PCFX_LATCH         DIGITAL_PIN_16
+#define PCFX_CLOCK         DIGITAL_PIN_15
+#define PCFX_DATA          DIGITAL_PIN_14
 
-#define ThreeDO_LATCH      DIGITAL_PIN_02
-#define ThreeDO_DATA       DIGITAL_PIN_04
-#define ThreeDO_CLOCK      DIGITAL_PIN_03
+#define SS_SELECT0         DIGITAL_PIN_20
+#define SS_SEL             DIGITAL_PIN_20
+#define SS_SELECT1         DIGITAL_PIN_21
+#define SS_REQ             DIGITAL_PIN_21
+#define SS_ACK             DIGITAL_PIN_17
+#define SS_DATA0           DIGITAL_PIN_17
+#define SS_DATA1           DIGITAL_PIN_16
+#define SS_DATA2           DIGITAL_PIN_15
+#define SS_DATA3           DIGITAL_PIN_14
 
-#define PCFX_LATCH         DIGITAL_PIN_03
-#define PCFX_CLOCK         DIGITAL_PIN_04
-#define PCFX_DATA          DIGITAL_PIN_05
+#define TG_SELECT          DIGITAL_PIN_20
+#define TG_DATA1           DIGITAL_PIN_17
+#define TG_DATA2           DIGITAL_PIN_16
+#define TG_DATA3           DIGITAL_PIN_15
+#define TG_DATA4           DIGITAL_PIN_14
 
-#define SS_SELECT0         DIGITAL_PIN_06
-#define SS_SEL             DIGITAL_PIN_06
-#define SS_SELECT1         DIGITAL_PIN_07
-#define SS_REQ             DIGITAL_PIN_07
-#define SS_ACK             DIGITAL_PIN_08
-#define SS_DATA0           DIGITAL_PIN_02
-#define SS_DATA1           DIGITAL_PIN_03
-#define SS_DATA2           DIGITAL_PIN_04
-#define SS_DATA3           DIGITAL_PIN_05
-
-#define TG_SELECT          DIGITAL_PIN_06
-#define TG_DATA1           DIGITAL_PIN_02
-#define TG_DATA2           DIGITAL_PIN_03
-#define TG_DATA3           DIGITAL_PIN_04
-#define TG_DATA4           DIGITAL_PIN_05
-
-#define PS_ATT             DIGITAL_PIN_02
-#define PS_CLOCK           DIGITAL_PIN_03
-#define PS_ACK             DIGITAL_PIN_04
-#define PS_CMD             DIGITAL_PIN_05
-#define PS_DATA            DIGITAL_PIN_06
+#define PS_ATT             DIGITAL_PIN_17
+#define PS_CLOCK           DIGITAL_PIN_16
+#define PS_ACK             DIGITAL_PIN_15
+#define PS_CMD             DIGITAL_PIN_14
+#define PS_DATA            DIGITAL_PIN_20
 
 //PORTD
-#define NEOGEO_SELECT      DIGITAL_PIN_02
-#define NEOGEO_D           DIGITAL_PIN_03
-#define NEOGEO_B           DIGITAL_PIN_04
-#define NEOGEO_RIGHT       DIGITAL_PIN_05
-#define NEOGEO_DOWN        DIGITAL_PIN_06
-#define NEOGEO_START       DIGITAL_PIN_07
+#define NEOGEO_SELECT      DIGITAL_PIN_17
+#define NEOGEO_D           DIGITAL_PIN_16
+#define NEOGEO_B           DIGITAL_PIN_15
+#define NEOGEO_RIGHT       DIGITAL_PIN_14
+#define NEOGEO_DOWN        DIGITAL_PIN_20
+#define NEOGEO_START       DIGITAL_PIN_21
 //PORTB
-#define NEOGEO_C           DIGITAL_PIN_08
-#define NEOGEO_A           DIGITAL_PIN_09
-#define NEOGEO_LEFT        DIGITAL_PIN_10
-#define NEOGEO_UP          DIGITAL_PIN_11
+#define NEOGEO_C           DIGITAL_PIN_17
+#define NEOGEO_A           DIGITAL_PIN_07
+#define NEOGEO_LEFT        DIGITAL_PIN_18
+#define NEOGEO_UP          DIGITAL_PIN_19
 
-#define INTPIN1            DIGITAL_PIN_02
-#define INTPIN2            DIGITAL_PIN_03
-#define INTPIN3            DIGITAL_PIN_04
-#define INTPIN4            DIGITAL_PIN_05
+#define INTPIN1            DIGITAL_PIN_17
+#define INTPIN2            DIGITAL_PIN_16
+#define INTPIN3            DIGITAL_PIN_15
+#define INTPIN4            DIGITAL_PIN_14
 #define INTPIN5			       NOT_CONNECTED
-#define INTPIN6            DIGITAL_PIN_07
-#define INTPIN7            DIGITAL_PIN_10
-#define INTPIN8            DIGITAL_PIN_11
-#define INTPIN9            DIGITAL_PIN_08
+#define INTPIN6            DIGITAL_PIN_21
+#define INTPIN7            DIGITAL_PIN_18
+#define INTPIN8            DIGITAL_PIN_19
+#define INTPIN9            DIGITAL_PIN_17
 
-#define AJ_COLUMN1         DIGITAL_PIN_08
-#define AJ_COLUMN2         DIGITAL_PIN_09
-#define AJ_COLUMN3		     DIGITAL_PIN_10
-#define AJ_COLUMN4         DIGITAL_PIN_11
+#define AJ_COLUMN1         DIGITAL_PIN_17
+#define AJ_COLUMN2         DIGITAL_PIN_07
+#define AJ_COLUMN3		     DIGITAL_PIN_18
+#define AJ_COLUMN4         DIGITAL_PIN_19
 
-#define SMS_INPUT_PIN_0    DIGITAL_PIN_02
-#define SMS_INPUT_PIN_1    DIGITAL_PIN_03
-#define SMS_INPUT_PIN_2    DIGITAL_PIN_04
-#define SMS_INPUT_PIN_3    DIGITAL_PIN_05
-#define SMS_INPUT_PIN_4    DIGITAL_PIN_07
-#define SMS_INPUT_PIN_5    DIGITAL_PIN_08 + PORTB_PIN_OFFSET
+#define SMS_INPUT_PIN_0    17
+#define SMS_INPUT_PIN_1    16
+#define SMS_INPUT_PIN_2    15
+#define SMS_INPUT_PIN_3    14
+#define SMS_INPUT_PIN_4    20
+#define SMS_INPUT_PIN_5    21
 
-#define SMSONGEN_INPUT_PIN_0    DIGITAL_PIN_02
-#define SMSONGEN_INPUT_PIN_1    DIGITAL_PIN_03
-#define SMSONGEN_INPUT_PIN_2    DIGITAL_PIN_04
-#define SMSONGEN_INPUT_PIN_3    DIGITAL_PIN_05
-#define SMSONGEN_INPUT_PIN_4    DIGITAL_PIN_06
-#define SMSONGEN_INPUT_PIN_5    DIGITAL_PIN_07
+#define SMSONGEN_INPUT_PIN_0    17
+#define SMSONGEN_INPUT_PIN_1    16
+#define SMSONGEN_INPUT_PIN_2    15
+#define SMSONGEN_INPUT_PIN_3    14
+#define SMSONGEN_INPUT_PIN_4    20
+#define SMSONGEN_INPUT_PIN_5    21
 
-#define GENESIS_TH            DIGITAL_PIN_08
-#define GENESIS_TR            DIGITAL_PIN_07
-#define GENESIS_TL            DIGITAL_PIN_06
+#define GENESIS_TH            DIGITAL_PIN_17
+#define GENESIS_TR            DIGITAL_PIN_21
+#define GENESIS_TL            DIGITAL_PIN_20
 
 #define PIND_READ( pin ) ((PORTD.IN << 2)&(1<<(pin)))
 #define PINB_READ( pin ) (PORTA.IN & (1<<(pin)))
