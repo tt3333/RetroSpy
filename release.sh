f01b02ee9342e2ec19b58e3f925466923ac4da3c
Christopher J Mallery <zoggins@retro-spy.com>
2023-01-03 17:14:17 -0800

    testing

--- release.sh
+++ release.sh
@@ -6,32 +6,33 @@ rm -rf bin/Release/net7.0
 rm -rf bin/Release/RetroSpy-Linux
 rm -rf RetroSpy-Linux.tar.gz
 
-dotnet build RetroSpyX/RetroSpyX.csproj /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=../bin/Release/net7.0
+#dotnet build RetroSpyX/RetroSpyX.csproj /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=../bin/Release/net7.0
 
 if [ $? -ne 0 ] 
 then 
    echo "Aborting release. Error during RetroSpyX build."
 else
-   dotnet build GBPemuX/GBPemuX.csproj /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=../bin/Release/net7.0
+   #dotnet build GBPemuX/GBPemuX.csproj /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=../bin/Release/net7.0
    if [ $? -ne 0 ] 
    then 
      echo "Aborting release. Error during GBPemuX build."
    else
-     dotnet build GBPUpdaterX/GBPUpdaterX.csproj /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=../bin/Release/net7.0
+     #dotnet build GBPUpdaterX/GBPUpdaterX.csproj /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=../bin/Release/net7.0
      if [ $? -ne 0 ] 
      then 
        echo "Aborting release. Error during GBPUpdater build."
      else
-       dotnet build UsbUpdaterX/UsbUpdaterX.csproj /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=../bin/Release/net7.0
+       #dotnet build UsbUpdaterX/UsbUpdaterX.csproj /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=../bin/Release/net7.0
        if [ $? -ne 0 ] 
        then 
          echo "Aborting release. Error during GBPUpdater build."
        else
          cd bin/Release/
-         cp -r net7.0 RetroSpy-Linux
-	     tar -zcvf ../../RetroSpy-Linux.tar.gz RetroSpy-Linux
+         #cp -r net7.0 RetroSpy-Linux
+	     #tar -zcvf ../../RetroSpy-Linux.tar.gz RetroSpy-Linux
          if [ -d "/mnt/src/upload" ]
          then
+           echo HI
            rm -rf /mnt/src/upload/RetroSpy-Linux.tar.gz
            cp ../../RetroSpy-Linux.tar.gz /mnt/src/upload  
          fi
