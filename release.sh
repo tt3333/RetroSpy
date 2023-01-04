6a6aed63edcf450a26b3d5099fc3235b878e8542
Christopher J Mallery <zoggins@retro-spy.com>
2023-01-03 17:15:10 -0800

    testing revert

--- release.sh
+++ release.sh
@@ -6,30 +6,30 @@ rm -rf bin/Release/net7.0
 rm -rf bin/Release/RetroSpy-Linux
 rm -rf RetroSpy-Linux.tar.gz
 
-#dotnet build RetroSpyX/RetroSpyX.csproj /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=../bin/Release/net7.0
+dotnet build RetroSpyX/RetroSpyX.csproj /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=../bin/Release/net7.0
 
 if [ $? -ne 0 ] 
 then 
    echo "Aborting release. Error during RetroSpyX build."
 else
-   #dotnet build GBPemuX/GBPemuX.csproj /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=../bin/Release/net7.0
+   dotnet build GBPemuX/GBPemuX.csproj /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=../bin/Release/net7.0
    if [ $? -ne 0 ] 
    then 
      echo "Aborting release. Error during GBPemuX build."
    else
-     #dotnet build GBPUpdaterX/GBPUpdaterX.csproj /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=../bin/Release/net7.0
+     dotnet build GBPUpdaterX/GBPUpdaterX.csproj /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=../bin/Release/net7.0
      if [ $? -ne 0 ] 
      then 
        echo "Aborting release. Error during GBPUpdater build."
      else
-       #dotnet build UsbUpdaterX/UsbUpdaterX.csproj /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=../bin/Release/net7.0
+       dotnet build UsbUpdaterX/UsbUpdaterX.csproj /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=../bin/Release/net7.0
        if [ $? -ne 0 ] 
        then 
          echo "Aborting release. Error during GBPUpdater build."
        else
          cd bin/Release/
-         #cp -r net7.0 RetroSpy-Linux
-	     #tar -zcvf ../../RetroSpy-Linux.tar.gz RetroSpy-Linux
+         cp -r net7.0 RetroSpy-Linux
+	     tar -zcvf ../../RetroSpy-Linux.tar.gz RetroSpy-Linux
          if [ -d "/mnt/src/upload" ]
          then
            echo HI
