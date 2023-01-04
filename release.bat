32fcb98ce57b0d2279bd9e09bcbc28d675023c24
Christopher J Mallery <zoggins@retro-spy.com>
2023-01-03 17:30:00 -0800

    Stupid mistake in release script

--- release.bat
+++ release.bat
@@ -117,13 +117,13 @@ copy GBP_Firmware.zip RetroSpy-Upload
 if exist "..\..\..\kernel\kernel.tar.gz" (
 copy ..\..\..\kernel\kernel.tar.gz RetroSpy-Upload
 )
-if exist "..\..\..\beaglebone\" (
-FOR /F %%I IN ('DIR ..\..\..\beaglebone\*.xz /B /O:-D') DO COPY ..\..\..\beaglebone\%%I RetroSpy-Upload & goto end
-)
 if exist "..\..\..\upload\RetroSpy-Linux.tar.gz" (
 copy ..\..\..\upload\RetroSpy-Linux.tar.gz RetroSpy-Upload
 )
 if exist "..\..\..\upload\RetroSpy-macOS.zip" (
 copy ..\..\..\upload\RetroSpy-macOS.zip RetroSpy-Upload
+)
+if exist "..\..\..\beaglebone\" (
+FOR /F %%I IN ('DIR ..\..\..\beaglebone\*.xz /B /O:-D') DO COPY ..\..\..\beaglebone\%%I RetroSpy-Upload & goto end
 )
 :end
