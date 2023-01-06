2c40d25c21bc3e6f27b5ebb239648b1942d666cf
Christopher J Mallery <zoggins@retro-spy.com>
2023-01-06 14:24:22 -0800

    Mac binaries are now signed and noterized

--- release-mac.sh
+++ release-mac.sh
@@ -30,7 +30,15 @@ else
         else
             cd bin/Release/
             cp -r net7.0 RetroSpy-macOS-x64
-	        zip -r ../../RetroSpy-macOS-x64.zip RetroSpy-macOS-x64
+            codesign --force --verbose --timestamp --sign "USGM6XTVY2" --options=runtime --entitlements ../../entitlements.plist RetroSpy-macOS-x64/*.dylib
+            codesign --force --verbose --timestamp --sign "USGM6XTVY2" --options=runtime --entitlements ../../entitlements.plist RetroSpy-macOS-x64/osx.os
+            codesign --force --verbose --timestamp --sign "USGM6XTVY2" --options=runtime --entitlements ../../entitlements.plist RetroSpy-macOS-x64/createdump
+            codesign --force --verbose --timestamp --sign "USGM6XTVY2" --options=runtime --entitlements ../../entitlements.plist RetroSpy-macOS-x64/RetroSpy
+            codesign --force --verbose --timestamp --sign "USGM6XTVY2" --options=runtime --entitlements ../../entitlements.plist RetroSpy-macOS-x64/GBPemu
+            codesign --force --verbose --timestamp --sign "USGM6XTVY2" --options=runtime --entitlements ../../entitlements.plist RetroSpy-macOS-x64/GBPUpdater
+            codesign --force --verbose --timestamp --sign "USGM6XTVY2" --options=runtime --entitlements ../../entitlements.plist RetroSpy-macOS-x64/USBUpdater
+	    ditto -c --sequesterRsrc -k -V RetroSpy-macOS-x64/ ../../RetroSpy-macOS-x64.zip
+            xcrun notarytool submit ../../RetroSpy-macOS-x64.zip --wait --apple-id "christopher.mallery@gmail.com" --password "icza-urwv-wmny-mvkf" --team-id "USGM6XTVY2" --output-format json
             if [ -d "/Volumes/src/upload" ]
             then
             cp ../../RetroSpy-macOS-x64.zip /Volumes/src/upload  
@@ -40,6 +48,8 @@ else
         fi
     fi
 fi
+
+rm -rf bin/Release/net7.0
 
 dotnet build RetroSpyX/RetroSpyX.csproj /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=../bin/Release/net7.0 -r osx-arm64
 if [ $? -ne 0 ] 
@@ -63,7 +73,15 @@ else
         else
             cd bin/Release/
             cp -r net7.0 RetroSpy-macOS-arm64
-	        zip -r ../../RetroSpy-macOS-arm64.zip RetroSpy-macOS-arm64
+            codesign --force --verbose --timestamp --sign "USGM6XTVY2" --options=runtime --entitlements ../../entitlements.plist RetroSpy-macOS-arm64/*.dylib
+            codesign --force --verbose --timestamp --sign "USGM6XTVY2" --options=runtime --entitlements ../../entitlements.plist RetroSpy-macOS-arm64/osx.os
+            codesign --force --verbose --timestamp --sign "USGM6XTVY2" --options=runtime --entitlements ../../entitlements.plist RetroSpy-macOS-arm64/createdump
+            codesign --force --verbose --timestamp --sign "USGM6XTVY2" --options=runtime --entitlements ../../entitlements.plist RetroSpy-macOS-arm64/RetroSpy
+            codesign --force --verbose --timestamp --sign "USGM6XTVY2" --options=runtime --entitlements ../../entitlements.plist RetroSpy-macOS-arm64/GBPemu
+            codesign --force --verbose --timestamp --sign "USGM6XTVY2" --options=runtime --entitlements ../../entitlements.plist RetroSpy-macOS-arm64/GBPUpdater
+            codesign --force --verbose --timestamp --sign "USGM6XTVY2" --options=runtime --entitlements ../../entitlements.plist RetroSpy-macOS-arm64/USBUpdater
+	        ditto -c --sequesterRsrc -k -V RetroSpy-macOS-arm64/ ../../RetroSpy-macOS-arm64.zip
+            xcrun notarytool submit ../../RetroSpy-macOS-arm64.zip --wait --apple-id "christopher.mallery@gmail.com" --password "icza-urwv-wmny-mvkf" --team-id "USGM6XTVY2" --output-format json
             if [ -d "/Volumes/src/upload" ]
             then
             cp ../../RetroSpy-macOS-arm64.zip /Volumes/src/upload  
