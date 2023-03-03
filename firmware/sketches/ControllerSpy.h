fc8ec7c27e87c41abc4357dfc1e53e5701bab594
Christopher J Mallery <zoggins@retro-spy.com>
2023-03-03 11:22:29 -0800

    Bumping to 6.1.3

--- firmware/sketches/ControllerSpy.h
+++ firmware/sketches/ControllerSpy.h
@@ -44,7 +44,7 @@ public:
 		Serial.print(startupMsg());
 		Serial.println(" mode");
 		Serial.print("// Version: ");
-		Serial.println("6.1.2"); /*VERSIONINFO*/
+		Serial.println("6.1.3"); /*VERSIONINFO*/
 		delay(1000);		
 	}
 	
